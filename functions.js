a6dda4cfadc6d040a20e926e8a3f28dfa1bf4377
nithaamjulius <nithaamjulius06@gmail.com>
2026-05-27 12:18:43 +0200

    Updated functions.js

--- functions.js
+++ functions.js
@@ -1,30 +1,28 @@
 // TODO: Create a function called greet that takes a name parameter
-
-function greet(name) {}
-
 // TODO: Make the greet function return "Hello, [name]!"
 
 function greet(name) {
-  return "Hello, [name]!";
+return "Hello, " + name + "!";
 }
 
 // TODO: Create a function called add that takes two parameters
-
-function add(parameter1, parameter) {}
-
 // TODO: Make the add function return the sum of both parameters
 
 function add(parameter1, parameter2) {
-  return parameter1 + parameter2;
+return parameter1 + parameter2;
 }
 
 // TODO: Call both functions and store their results in variables
 
-add(10, 5);
+const greeting = greet("Alice");
+
+const sum = add(10, 5);
 
 // TODO: Print the results to the console
 
-console.log(add(10, 5));
+console.log(greeting);
+console.log(sum);
+
 
 //  Who did what between Nithaam and Chad
 
