4d2927b6c5e32b2e77bf3f6b0a3be490ba44fe17
nithaamjulius <nithaamjulius06@gmail.com>
2026-05-27 12:05:28 +0200

    initial commit

---
+++ functions.js
@@ -0,0 +1,35 @@
+// TODO: Create a function called greet that takes a name parameter
+
+function greet(name) {}
+
+// TODO: Make the greet function return "Hello, [name]!"
+
+function greet(name) {
+  return "Hello, [name]!";
+}
+
+// TODO: Create a function called add that takes two parameters
+
+function add(parameter1, parameter) {}
+
+// TODO: Make the add function return the sum of both parameters
+
+function add(parameter1, parameter2) {
+  return parameter1 + parameter2;
+}
+
+// TODO: Call both functions and store their results in variables
+
+add(10, 5);
+
+// TODO: Print the results to the console
+
+console.log(add(10, 5));
+
+//  Who did what between Nithaam and Chad
+
+// Nithaam and Chad did code such as giving variable names and values and doing the TODOs
+
+// Nithaam uploaded files to GitHub
+
+// Chad made and uploaded README.md
